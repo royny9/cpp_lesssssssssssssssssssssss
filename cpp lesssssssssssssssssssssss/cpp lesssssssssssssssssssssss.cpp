29a83f25f65bae1ffcfcb4b6b753aadb0f848749
royny <99902434+royny9@users.noreply.github.com>
2024-09-14 11:53:02 +0300

    лаб 2 задание 1

--- cpp lesssssssssssssssssssssss/cpp lesssssssssssssssssssssss.cpp
+++ cpp lesssssssssssssssssssssss/cpp lesssssssssssssssssssssss.cpp
@@ -1,20 +1,104 @@
-﻿// cpp lesssssssssssssssssssssss.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿
+
+#include <iostream> // библиотека дял работы с потоками ввода вывода 
+using namespace std;// избавляемся от stg::
+
+
+//int main()// обозначение главной функции
+//{
+//    setlocale(LC_ALL, "Rus");// боюавляем русский язык
+//    cout << "first string" << endl; // задание 1 
 //
+//    cout << "Первая строка \n Вторая строка\n"; // задание 2
+//    string s = "спецсимовлы \"\\\""; //задание 3
+//    cout << s << endl; // вывод задания 
+//    int a = 150;
+//    float b = 15.933;
+//    int c = 250;
+//    cout << "a = " << a << "b = " << b << "c = " << c << endl;
+//    int d = 3;
+//    int m = 3;
+//    int g = 2005;
+//    cout << "моя дата рождения: " << d << m << g << endl;
+// 
+//} //конец 
 
-#include <iostream>
+//int main() {
+//	int y;
+//	cout << "vvedite chisllo: ";
+//	cin >> y;
+//	/*if (y < 10) {
+//		cout << (y * 2);
+//	}
+//	else {
+//		cout << (y / 10);
+//	}*/ // ОБЫЧНЫЙ УСЛОВНЫЙ ОПЕРАТОР 
+//	y = (y < 10) ? (y * 2) : (y / 10); // ТЕРНАРНЫЙ УСЛОВНЫЙ ОТПЕРАТОР
+//	cout << y;
+//}
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// ЛАБ РАБОТА 2
+//int main() {
+//	cout << "enter 2 nums: ";
+//	int num_1;
+//	int num_2;
+//	cin >> num_1 >> num_2;
+//	cout << "sr_znach = " << ((num_1 + num_2) / 2)<< endl;
+//	string operation;
+//	cout << "enter operation: (*,/,+,-)" << endl;
+//	cin >> operation;
+//	if (operation == "*") {
+//		cout << (num_1 * num_2);
+//	}
+//	else if (operation == "/") {
+//		cout << (num_1 / num_2);
+//	}
+//	else if (operation == "+") {
+//		cout << (num_1 + num_2);
+//	}
+//	else {
+//		cout << (num_1 - num_2);
+//	}
+//}
+
+/*int main() {
+	cout << "enter 2 nums: ";
+	int num_1;
+	int num_2;
+	cin >> num_1 >> num_2;
+	cout << "sr_znach = " << ((num_1 + num_2) / 2)<< endl;
+	char operation;
+	cout << "enter operation: (*,/,+,-)" << endl;
+	cin >> operation;
+	switch (operation) {
+	case '+': {
+		cout << (num_1 + num_2) << endl;
+		break;
+	}
+	case '-': {
+		cout << (num_1 - num_2) << endl;
+		break;
+	}
+	case '*': {
+		cout << (num_1 * num_2) << endl;
+		break;
+	}
+	case '/': {
+		cout << (num_1 / num_2) << endl;
+		break;
+	}
+	 }
+}*/
+int main() {
+	int num = 0;
+	while (true){
+		if (num < 1) {
+			cout << "Enter positive 1 num\n";
+			cin >> num;
+		}
+		else {
+			break;
+		}
+	}
+	cout << num;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
