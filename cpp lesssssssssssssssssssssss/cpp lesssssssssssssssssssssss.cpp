c335e3a9d40c750fd1a0bb510004d297e075ce9e
royny <99902434+royny9@users.noreply.github.com>
2024-09-26 13:19:14 +0300

    commit

--- cpp lesssssssssssssssssssssss/cpp lesssssssssssssssssssssss.cpp
+++ cpp lesssssssssssssssssssssss/cpp lesssssssssssssssssssssss.cpp
@@ -39,29 +39,29 @@ using namespace std;// избавляемся от stg::
 //}
 
 // ЛАБ РАБОТА 2
-// _________________задание 1_____________
-//int main() {
-//	cout << "enter 2 nums: ";
-//	int num_1;
-//	int num_2;
-//	cin >> num_1 >> num_2;
-//	cout << "sr_znach = " << ((num_1 + num_2) / 2)<< endl;
-//	string operation;
-//	cout << "enter operation: (*,/,+,-)" << endl;
-//	cin >> operation;
-//	if (operation == "*") {
-//		cout << (num_1 * num_2);
-//	}
-//	else if (operation == "/") {
-//		cout << (num_1 / num_2);
-//	}
-//	else if (operation == "+") {
-//		cout << (num_1 + num_2);
-//	}
-//	else {
-//		cout << (num_1 - num_2);
-//	}
-//}
+ _________________задание 1_____________
+int main() {
+	cout << "enter 2 nums: ";
+	int num_1;
+	int num_2;
+	cin >> num_1 >> num_2;
+	cout << "sr_znach = " << ((num_1 + num_2) / 2)<< endl;
+	string operation;
+	cout << "enter operation: (*,/,+,-)" << endl;
+	cin >> operation;
+	if (operation == "*") {
+		cout << (num_1 * num_2);
+	}
+	else if (operation == "/") {
+		cout << (num_1 / num_2);
+	}
+	else if (operation == "+") {
+		cout << (num_1 + num_2);
+	}
+	else {
+		cout << (num_1 - num_2);
+	}
+}
 
 /*int main() {
 	cout << "enter 2 nums: ";
@@ -91,123 +91,123 @@ using namespace std;// избавляемся от stg::
 	}
 	 }
 }*/
-//____________________задание 2____________
-//int main() {
-//	bool flag = true;
-//	int num_1;
-//	while (flag) {
-//		cout << "enter num > 0\n";
-//		cin >> num_1;
-//		if (num_1 > 0) {
-//			flag = false;
-//		}
-//	}
-//	int sum_list = 0;
-//	int list[]{ 1,2,3,4,5,6,7,8,9,10 };
-//	for (int i = 0; i < num_1; i++) {
-//		sum_list += list[i];
-//	}
-//	cout << "vot summa chisel: " << sum_list << endl;
-//	for (int i = 0; i < 10; i++) {
-//		cout << list[i] << " ";
-//	}
-//	cout << endl;
-//	for (int i = 0; i < 10; i ++) {
-//		if (list[i] % 2 == 0) {
-//			cout << list[i] << " ";
-//		}
-//	}
-//	cout << endl;
-//	for (int i = 0; i < 10; i++) {
-//		if (list[i] % 2 == 1) {
-//			cout << list[i] << " ";
-//		}
-//	}
-//}
-// --------------задание 3---------------
-//int function_10(int num) {
-//	int sum_nums = 0;
-//	if (num <= 0) {
-//		return 0;
-//	}
-//	else {
-//		for (int i = 0; i <= num; i++) {
-//			sum_nums += i;
-//		}
-//		return sum_nums;
-//	}
-//}
-//
-//int test_func(int par) {
-//	par = 1;
-//	if (par <= 0) {
-//		return 0;
-//	}
-//	else {
-//		int sum = 0;
-//		for (int i = 0; i <= par; i++) {
-//			sum += i;
-//		}
-//		return sum;
-//	}
-//}
-//
-//int funcref(int a, int b, int& c, int& d)
-//{
-//	c = a + b;
-//	d = a * b;
-//	return c, d;
-//}
-//int main() {
-//	setlocale(LC_ALL, "Russian");
-//	int sum_array = 0;
-//	int A[2][3] = { {0,1,2},
-//					{3,4,5}};
-//	for (int i = 0; i < 2; i++) {
-//		for (int j = 0; j < 3; j++) {
-//			sum_array += A[i][j];
-//		}
-//	}
-//	cout << sum_array << endl;
-//	int sec_list[3];
-//	sec_list[0] = A[0][0] + A[1][0];
-//	sec_list[1] = A[0][1] + A[1][1];
-//	sec_list[2] = A[0][2] + A[1][2];
-//	for (int i = 0; i < 3; i++) {
-//		cout << sec_list[i];
-//	}
-//	float num = 20.84;
-//	float &rnum = num;
-//	float &lnum = num;
-//	rnum = 32;
-//	cout << endl;
-//	cout << num << " " << rnum << " " << lnum << " " << endl;
-//
-//	int number_for_10;
-//	cout << "Enter your num: ";
-//	cin >> number_for_10;
-//	int fnc_10 = function_10(number_for_10);
-//	int test_f = test_func(number_for_10);
-//	cout << "resoult: " << fnc_10 << "\n" << "test whith per 1: " << test_f << endl;
-//
-//
-//	int num1, num2, sum_nums, mn;
-//	cout << "Введите число a: ";
-//	cin >> num1;
-//	cout << "Введите число b: ";
-//	cin >> num2;
-//	funcref(num1, num2, sum_nums, mn);
-//
-//	cout << "c = a + b = " << sum_nums << endl;
-//	cout << "d = a * b = " << mn << endl;
-//
-//	string st = "pisyatdva";
-//	for (int i = 0; i < 3; i++) {
-//		string st_sec = "RAZYEZD_CHUSHPANI";
-//		cout << st << endl;
-//		cout << st_sec << endl;
-//	}
-//}
+____________________задание 2____________
+int main() {
+	bool flag = true;
+	int num_1;
+	while (flag) {
+		cout << "enter num > 0\n";
+		cin >> num_1;
+		if (num_1 > 0) {
+			flag = false;
+		}
+	}
+	int sum_list = 0;
+	int list[]{ 1,2,3,4,5,6,7,8,9,10 };
+	for (int i = 0; i < num_1; i++) {
+		sum_list += list[i];
+	}
+	cout << "vot summa chisel: " << sum_list << endl;
+	for (int i = 0; i < 10; i++) {
+		cout << list[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < 10; i ++) {
+		if (list[i] % 2 == 0) {
+			cout << list[i] << " ";
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < 10; i++) {
+		if (list[i] % 2 == 1) {
+			cout << list[i] << " ";
+		}
+	}
+}
+ --------------задание 3---------------
+int function_10(int num) {
+	int sum_nums = 0;
+	if (num <= 0) {
+		return 0;
+	}
+	else {
+		for (int i = 0; i <= num; i++) {
+			sum_nums += i;
+		}
+		return sum_nums;
+	}
+}
+
+int test_func(int par) {
+	par = 1;
+	if (par <= 0) {
+		return 0;
+	}
+	else {
+		int sum = 0;
+		for (int i = 0; i <= par; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+}
+
+int funcref(int a, int b, int& c, int& d)
+{
+	c = a + b;
+	d = a * b;
+	return c, d;
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int sum_array = 0;
+	int A[2][3] = { {0,1,2},
+					{3,4,5}};
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			sum_array += A[i][j];
+		}
+	}
+	cout << sum_array << endl;
+	int sec_list[3];
+	sec_list[0] = A[0][0] + A[1][0];
+	sec_list[1] = A[0][1] + A[1][1];
+	sec_list[2] = A[0][2] + A[1][2];
+	for (int i = 0; i < 3; i++) {
+		cout << sec_list[i];
+	}
+	float num = 20.84;
+	float &rnum = num;
+	float &lnum = num;
+	rnum = 32;
+	cout << endl;
+	cout << num << " " << rnum << " " << lnum << " " << endl;
+
+	int number_for_10;
+	cout << "Enter your num: ";
+	cin >> number_for_10;
+	int fnc_10 = function_10(number_for_10);
+	int test_f = test_func(number_for_10);
+	cout << "resoult: " << fnc_10 << "\n" << "test whith per 1: " << test_f << endl;
+
+
+	int num1, num2, sum_nums, mn;
+	cout << "Введите число a: ";
+	cin >> num1;
+	cout << "Введите число b: ";
+	cin >> num2;
+	funcref(num1, num2, sum_nums, mn);
+
+	cout << "c = a + b = " << sum_nums << endl;
+	cout << "d = a * b = " << mn << endl;
+
+	string st = "pisyatdva";
+	for (int i = 0; i < 3; i++) {
+		string st_sec = "RAZYEZD_CHUSHPANI";
+		cout << st << endl;
+		cout << st_sec << endl;
+	}
+}
 //_____________________задание 4_______________
 
 //1.   Создайте функцию с одним параметром, внутри которой объявите статическую переменную с начальным значением равным 0.
---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+void printArr(int* begin, int* end);
+void diff(int a, int d);
+void summ(int a, int d);
+void chfunc(char ch);
+
+int main() {
+	int* point;
+	int x = 52;
+	point = &x;
+
+
+	cout << "point = " << point<< endl;
+	cout << "*point = " << *point << endl;
+
+	*point = 1488;
+	cout << "x = " << x << "point = " << *point << endl;
+	
+	
+	int array[5] = { 1,2,3,4,5 };
+	for (int* i = array; i != array + 5; i++) {
+		cout << *i << endl;
+	}
+
+
+	int num2 = 5;
+	const int* point1;
+	point1 = &num2;
+	
+	cout << point1 << " " << *point1 << endl;
+	num2 = 10;
+	cout << point1 << " " << *point1 << endl;
+	//point1 = &x;// ошибка
+	
+	int array1[5] = { 1,2,3,4,5 };
+	printArr(begin(array1), end(array1));
+	cout << "____________" << endl;
+	
+	int a = 15;
+	int b = 10;
+	diff(a, b);
+	summ(a, b);
+
+}
+
+void diff(int a, int d) {
+	cout << (a - d) << endl;
+}
+
+void summ(int a, int d) {
+	cout << (a + d) << endl;
+}
+
+void printArr(int* begin, int* end) {
+	int sum_array = 0;
+	for (int* i = begin; i < end; i++) {
+		sum_array += *i;
+	}
+	cout << sum_array << endl;
+}
